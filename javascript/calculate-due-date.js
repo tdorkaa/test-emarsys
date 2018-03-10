b1c7f1744776fbe430a13c9a77147e49587a2ffa
Dorottya <dorottya.tamas@ge.com>
2018-03-10 20:17:49 +0100

    Step 6: handle weekend due date

--- javascript/calculate-due-date.js
+++ javascript/calculate-due-date.js
@@ -1,16 +1,10 @@
 const WORK_DAY_START = 9;
 const WORK_DAY_END = 17;
 const WORK_DAY_LENGTH = WORK_DAY_END - WORK_DAY_START;
+const WORK_WEEK_LENGTH = 5;
 
-function calculateDueDate(submitDate, turnAroundTime) {
-    let dueDate = new Date(submitDate.getTime());
-
-    let daysToAdd = Math.floor(turnAroundTime / WORK_DAY_LENGTH);
-    daysToAdd = daysToAdd + Math.floor(daysToAdd/5)*2;
+function addHours(turnAroundTime, dueDate) {
     let hoursToAdd = turnAroundTime % WORK_DAY_LENGTH;
-
-    dueDate.setDate(dueDate.getDate() + daysToAdd);
-
     let hourOfDay = dueDate.getHours() + hoursToAdd;
     if (hourOfDay > WORK_DAY_END) {
         dueDate.setDate(dueDate.getDate() + 1);
@@ -18,5 +12,22 @@ function calculateDueDate(submitDate, turnAroundTime) {
     } else {
         dueDate.setHours(hourOfDay);
     }
+}
+
+function addDays(turnAroundTime, dueDate) {
+    let daysToAdd = Math.floor(turnAroundTime / WORK_DAY_LENGTH);
+    daysToAdd = daysToAdd + Math.floor(daysToAdd / WORK_WEEK_LENGTH) * 2;
+    dueDate.setDate(dueDate.getDate() + daysToAdd);
+
+    let dayOfWeek = dueDate.getDay();
+    if (dayOfWeek > WORK_WEEK_LENGTH) {
+        dueDate.setDate(dueDate.getDate() + 7 - dayOfWeek + 1);
+    }
+}
+
+function calculateDueDate(submitDate, turnAroundTime) {
+    let dueDate = new Date(submitDate.getTime());
+    addHours(turnAroundTime, dueDate);
+    addDays(turnAroundTime, dueDate);
     return dueDate;
 };
--- spec/calculate-due-date.spec.js
+++ spec/calculate-due-date.spec.js
@@ -1,48 +1,62 @@
 describe('calculateDueDate', function(){
     let submitDate;
-    let turnAroudTimeInHours;
+    let turnAroundTimeInHours;
     let dueDate;
 
     it('should calculate the due date to be 2018-03-12 12AM when the submit date: 2018-03-12 (Monday) 10AM and the turnaroudtime: 2 hours', function() {
         submitDate = new Date('2018-03-12T10:00:00');
-        turnAroudTimeInHours = 2;
+        turnAroundTimeInHours = 2;
         dueDate = new Date('2018-03-12T12:00:00');
-        expect(calculateDueDate(submitDate, turnAroudTimeInHours)).toEqual(dueDate);
+        expect(calculateDueDate(submitDate, turnAroundTimeInHours)).toEqual(dueDate);
     });
 
     it('should calculate the due date to be 2018-03-13 12AM when the submit date: 2018-03-12 (Monday) 10AM and the turnaroudtime: 10 hours', function() {
         submitDate = new Date('2018-03-12T10:00:00');
         dueDate = new Date('2018-03-13T12:00:00');
-        turnAroudTimeInHours = 10;
-        expect(calculateDueDate(submitDate, turnAroudTimeInHours)).toEqual(dueDate);
+        turnAroundTimeInHours = 10;
+        expect(calculateDueDate(submitDate, turnAroundTimeInHours)).toEqual(dueDate);
     });
 
     it('should calculate the due date to be 2018-03-14 10AM when the submit date: 2018-03-12 (Monday) 4PM and the turnaroudtime: 10 hours', function() {
         submitDate = new Date('2018-03-12T16:00:00');
         dueDate = new Date('2018-03-14T10:00:00');
-        turnAroudTimeInHours = 10;
-        expect(calculateDueDate(submitDate, turnAroudTimeInHours)).toEqual(dueDate);
+        turnAroundTimeInHours = 10;
+        expect(calculateDueDate(submitDate, turnAroundTimeInHours)).toEqual(dueDate);
     });
 
     it('should calculate the due date to be 2018-03-14 4PM when the submit date: 2018-03-12 (Monday) 5PM and the turnaroudtime: 7 hours', function() {
         submitDate = new Date('2018-03-12T17:00:00');
         dueDate = new Date('2018-03-13T16:00:00');
-        turnAroudTimeInHours = 7;
-        expect(calculateDueDate(submitDate, turnAroudTimeInHours)).toEqual(dueDate);
+        turnAroundTimeInHours = 7;
+        expect(calculateDueDate(submitDate, turnAroundTimeInHours)).toEqual(dueDate);
     });
 
     it('should calculate the due date to be 2018-03-14 4PM when the submit date: 2018-03-12 (Monday) 5PM and the turnaroudtime: 40 hours', function() {
         submitDate = new Date('2018-03-12T17:00:00');
         dueDate = new Date('2018-03-19T17:00:00');
-        turnAroudTimeInHours = 40;
-        expect(calculateDueDate(submitDate, turnAroudTimeInHours)).toEqual(dueDate);
+        turnAroundTimeInHours = 40;
+        expect(calculateDueDate(submitDate, turnAroundTimeInHours)).toEqual(dueDate);
     });
 
     it('should calculate the due date to be 2018-03-27 2PM when the submit date: 2018-03-12 (Monday) 5PM and the turnaroudtime: 85 hours', function() {
         submitDate = new Date('2018-03-12T17:00:00');
         dueDate = new Date('2018-03-27T14:00:00');
-        turnAroudTimeInHours = 85;
-        expect(calculateDueDate(submitDate, turnAroudTimeInHours)).toEqual(dueDate);
+        turnAroundTimeInHours = 85;
+        expect(calculateDueDate(submitDate, turnAroundTimeInHours)).toEqual(dueDate);
+    });
+
+    it('should calculate the due date to be 2018-03-19 12AM when the submit date: 2018-03-16 (Friday) 5PM and the turnaroudtime: 3 hours', function() {
+        submitDate = new Date('2018-03-16T17:00:00');
+        dueDate = new Date('2018-03-19T12:00:00');
+        turnAroundTimeInHours = 3;
+        expect(calculateDueDate(submitDate, turnAroundTimeInHours)).toEqual(dueDate);
+    });
+
+    it('should calculate the due date to be 2018-03-19 12AM when the submit date: 2018-03-15 (Thursday) 5PM and the turnaroudtime: 11 hours', function() {
+        submitDate = new Date('2018-03-15T17:00:00');
+        dueDate = new Date('2018-03-19T12:00:00');
+        turnAroundTimeInHours = 11;
+        expect(calculateDueDate(submitDate, turnAroundTimeInHours)).toEqual(dueDate);
     });
 
 });
