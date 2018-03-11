d8bbc44e2c9f5e81b0532ce0e94aea85bd708a86
Dorottya <dorottya.tamas@ge.com>
2018-03-11 14:30:05 +0100

    Step 7: validate inputs

--- javascript/calculate-due-date.js
+++ javascript/calculate-due-date.js
@@ -25,9 +25,34 @@ function addDays(turnAroundTime, dueDate) {
     }
 }
 
+function validateSubmitDate(submitDate) {
+    if (!(submitDate instanceof Date)) {
+        throw 'Invalid submit date: should be an instance of Date';
+    }
+
+    let submitDay = submitDate.getDay();
+    let submitHour = submitDate.getHours();
+    if (submitDay > WORK_WEEK_LENGTH || WORK_DAY_START > submitHour || WORK_DAY_END < submitHour) {
+        throw 'Invalid submit date: should be during business hours.';
+    }
+}
+
+function validateTurnAroundTime(turnAroundTime) {
+    if (typeof turnAroundTime !== 'number' || turnAroundTime < 0 || turnAroundTime !== Math.floor(turnAroundTime)) {
+        throw 'Invalid turnAroundTime: should be a natural number';
+    }
+}
+
+function validateInputs(submitDate, turnAroundTime) {
+    validateTurnAroundTime(turnAroundTime);
+    validateSubmitDate(submitDate);
+}
+
 function calculateDueDate(submitDate, turnAroundTime) {
+    validateInputs(submitDate, turnAroundTime);
+
     let dueDate = new Date(submitDate.getTime());
     addHours(turnAroundTime, dueDate);
     addDays(turnAroundTime, dueDate);
     return dueDate;
-};
+}
